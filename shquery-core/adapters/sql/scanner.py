79a89cdcde65c2a511b42f3174df02945358a4c7
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-03-16 22:39:25 +1100

    initialise file discovery resolution process

--- shquery-core/adapters/sql/scanner.py
+++ shquery-core/adapters/sql/scanner.py
@@ -1,1 +1,39 @@
-# Scan sql files in repo as a start then be able to track down e.g. spring boot entities aswell
+# Current task:
+# Repo Scanner
+#     │
+#     ▼
+#File Discovery
+#     │
+#     ▼
+#Language Detection
+#     │
+#     ▼
+#AST Parser (per language) {call this only after we've filtered out files that don't contain any db related code}
+#     │
+#     ▼
+#Rule Engine
+#     │
+#     ▼
+#SQL Extraction
+# To improve performance, just make sure we skip venv, node_modules and all that stuff
+# Also use like a keyword prefilter saying like if SELECT or whatever dml not in file, skip file
+import os;
+
+def scan_codebase(directory):
+  sql_files=[] #to handle all .sql files 
+  migration_scripts=[] #to handle all *.migration.* files
+  java_files=[] # to handle JPA Entity
+  javascript_files=[]
+  typescript_files=[]
+  
+  
+  
+  for root,dirs, files in os.walk(directory):
+    for file in files:
+      path = os.path.join(root,file)
+      
+      # sql files
+      if file.endsWith("sql"):
+        sql_files.append(path)
+  
+  
