ebf08dea605648afd1e5561bd2dfc5006848c19a
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-03-17 23:48:23 +1100

    updated perspective on difficulty of tool

--- shquery-core/adapters/sql/scanner.py
+++ shquery-core/adapters/sql/scanner.py
@@ -17,6 +17,9 @@
 #SQL Extraction
 # To improve performance, just make sure we skip venv, node_modules and all that stuff
 # Also use like a keyword prefilter saying like if SELECT or whatever dml not in file, skip file
+# On discussion with people, I may have to use a custom annotation/interface for 100% coverage and to 
+# make it easier to detect sql queries... This'll work well for raw sql, but I still need to think about
+# jpql etc..
 import os;
 
 def scan_codebase(directory):
@@ -35,5 +38,14 @@ def scan_codebase(directory):
       # sql files
       if file.endsWith("sql"):
         sql_files.append(path)
+      
+      if file.endsWith(".java"):
+        java_files.append(path)
+        
+      if file.endsWith(".js"):
+        javascript_files.append(path)
+        
+      if file.endsWith(".ts"):
+        typescript_files.append(path)
   
   
