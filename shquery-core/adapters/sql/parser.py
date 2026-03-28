1d9e967ff44abc72e9e2ad74ae404b4b117cd6ee
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-03-28 23:32:12 +1100

    some more thoughts

--- shquery-core/adapters/sql/parser.py
+++ shquery-core/adapters/sql/parser.py
@@ -21,8 +21,12 @@ def extract_sql_queries(json_lines_file_path:str):
     with open(json_lines_file_path,'r') as log_file:
       # filter to catch sql queries only...
       # we could read the whole file and scan for certain keywords to filter for sql queries only
-      # but is there a more efficient way of filtering out only SQL queries?? ahh I don't think so,
+      # but is there a more efficient way of filtering out only SQL queries?? don't think so,
       # either way O(N) time complexity will be required...
+      
+      # current problem @ hand is, what is my plan to gather these SQL lines from our JSONL file
+      # extract them from the file either into a data structure, or a JSONL file and run EXPLAIN
+      # on queries efficiently, whether there is just a 1000 sql logs or 100,000.
       pass
   except FileNotFoundError:
     raise FileNotFoundError(f"{json_lines_file_path} not found!")
