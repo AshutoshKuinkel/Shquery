06fb525ab4cb972c0e0be482fe1d5c701b08c81c
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-04-03 23:55:41 +1100

    fix: refactor code, splitting into functions

--- shquery-core/adapters/sql/parser.py
+++ shquery-core/adapters/sql/parser.py
@@ -3,16 +3,23 @@
 # https://www.geeksforgeeks.org/python/read-json-file-using-python
 # https://www.geeksforgeeks.org/python/json-loads-in-python
 # DDL VS DML: https://www.geeksforgeeks.org/dbms/difference-between-ddl-and-dml-in-dbms
+# MD5: https://www.geeksforgeeks.org/python/md5-hash-python/
 
 import json;
 import os
+import hashlib;
 from dotenv import load_dotenv
 load_dotenv()
 
-# thought: set would be good for deduplication, as we avoid having exact same query more than once, also note set is basically dict w/out the keys, so still O(1) lookups..
-# Im thinking maybe we could store in dict, then deduplicate by storing in set, then add to dict again, or maybe just add to a set right away..
+#  the key worry is more so, how do I determine what makes 2 queries the same for deduplication...
 
-def extract_sql_queries(json_lines_file_path:str)->list:
+# approach:
+# filter to catch sql queries only...
+# reading log line by line, adding it to a dict w/ deduplication/normalisation. T:O(N) S:O(N)
+# then run EXPLAIN for costs plan... 
+# also add a count frequency after normalisation/deduplication which will help performance analysis, identifying hot queries/deciding what to optimise
+
+def extract_raw_sql_queries(json_lines_file_path:str)-> list[str]:
   """
   Extract SQL queries from JSONL ( `JSON-Lines <https://jsonltools.com/what-is-jsonl>`_) log file.
 
@@ -20,44 +27,56 @@ def extract_sql_queries(json_lines_file_path:str)->list:
     json_lines_file_path (string): (Path to the JSONL log file.)
     
   Returns:
-        dict: A list of dictionaries, each containing:
-            - 'sql' (str): The extracted SQL query
+        list[str]: Raw SQL strings, unfiltered and uncleaned.
   """
-  # approach:
-  # filter to catch sql queries only...
-  # reading log line by line, adding it to a dict w/ deduplication/normalisation. T:O(N) S:O(N)
-  # normalisation... & then run EXPLAIN for costs plan... 
-  # also add a count frequency after normalisation/deduplication which will help performance analysis, identifying hot queries/deciding what to optimise
-  
-  # this is only a list for testing purposes, but lookups in list take O(N) and in dicts take O(1) time, so change to dict for performance..
-  clean_queries = []
+  queries = []
   try:
     with open(json_lines_file_path,'r') as file:
       for line in file:
-        line = line.strip()
-        
         try:
           py_obj_query = json.loads(line)
         except json.JSONDecodeError:
           # more suitable to throw continue here instead of JSONDecodeErr, so whole pipeline doesn't break.
           continue  
         
-        dml_query = py_obj_query.get('message','')
-        if dml_query.startswith('execute'):
-          # TO DO: add normalisation/deduplication
-          sql = dml_query.split(":",1)[1].replace("\\n"," ").replace("\n"," ").strip()
-          # removing whitespace, we may be affecting performance by doing 2 passes...
-          sql = " ".join(sql.split())
-          clean_queries.append(sql)
+        query = py_obj_query.get('message','')
+        if query.startswith('execute'):
+          sql = query.split(":", 1)[1]
+          queries.append(sql)
         else:
           continue
-        
-    return clean_queries;  
-      
   except FileNotFoundError:
     raise FileNotFoundError(f"{json_lines_file_path} not found!")
   
+  return queries;  
+  
+def normalise_and_filter(queries: list[str]) -> list[str]:
+  '''
+    Normalise whitespace, lowercase, & filter out DDL statements.
+    
+    Args:
+      queries (list[str]): Raw SQL strings.
+
+    Returns:
+      list[str]: Cleaned DML-only SQL strings.
+  '''
+  clean_queries = []
+  
+  for q in queries:
+    sql = " ".join(q.split()).lower()
+    
+    if sql.split[0] in DDL_WORDS:
+      continue
+    
+    clean_queries.append(sql)
+    
+    return clean_queries
+  
+  
   
 if __name__ == "__main__":
-  print(extract_sql_queries(os.getenv('JSONL_FILE_PATH')))
+  print(extract_raw_sql_queries(os.getenv('JSONL_FILE_PATH')))
+  
+
+
   
