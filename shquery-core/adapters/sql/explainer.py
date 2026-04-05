fbe955f56ccb82c9f39f2325c49fd7f7a74d30dd
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-04-05 22:45:28 +1000

    initialise explainer file for running EXPLAIN

--- shquery-core/adapters/sql/explainer.py
+++ shquery-core/adapters/sql/explainer.py
@@ -1,3 +1,2 @@
-# When running EXPLAIN in Postgres, it returns plain text rows e.g.:
-# Seq Scan on users  (cost=0.00..18334.00 rows=1000000 width=68)
-# Thats just string. There's no structured JSON or object, only raw text. So explainer is just extracting useful numbers and words out of that string.
+# purpose: run EXPLAIN to receive quick & efficient cost plan on queries. Frontline checking before workload sim.
+# references:
