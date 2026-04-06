97a8c3644ebebe64c6f1475801d0e63b5c12fb70
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-04-06 23:37:45 +1000

    some thoughts for coming days

--- shquery-core/adapters/sql/explainer.py
+++ shquery-core/adapters/sql/explainer.py
@@ -1,2 +1,14 @@
 # purpose: run EXPLAIN to receive quick & efficient cost plan on queries. Frontline checking before workload sim.
 # references:
+import parser;
+
+# approach: loop through the final dictonary returned from build_query_stats() 
+# and run EXPLAIN on it to get cost plan:
+query_dict = parser.build_query_stats()
+
+# loop through just the query keys here, it's fine to run it on all queries. just a simple EXPLAIN, either way
+# O(N)..
+
+# return dict output, 
+# with cost plan for that query (reference it with the hash we created in tallied_queries function )
+# TO DO: see how this fits in with workload sims, to determine if dict output is right way to go..
